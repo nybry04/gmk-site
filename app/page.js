981b354e29a12a6a2834785e129151dee6932723
Alexander <alexandernpe@gmail.com>
2023-08-09 19:42:38 +0700

    update

--- app/page.js
+++ app/page.js
@@ -1,23 +1,61 @@
 'use client'
 
 import Image from "next/image"
+import { useState } from "react"
 
 export default function Index() {
+    const [index, setIndex] = useState(0)
+    const [modalShow, setModalShow] = useState(false)
     const items = [
         {
             image: '/gold.jpg',
             title: 'GOLD Random Key',
-            price: 259
+            price: 259,
+            description: `НАШИ ПОКУПАТЕЛИ ПРИЗНАЛИ НАШ GOLD RANDOM KEY ЛУЧШИМ РАНДОМОМ В РУНЕТЕ!<br/>
+            <br/>
+            Из этого кейса вы получите игру от 399 до 5499 рублей.<br/>
+            Мы вернем ваши деньги, если игра в Steam окажется дешевле без учета скидок.<br/>
+            <br/>
+            Покупая GOLD Random Key вы не только экономите деньги, но и имеете шанс получить дорогую игру, включая такие хиты, как: Cyberpunk 2077, GTA V, Red Dead Redemption 2, Elden Ring, Resident Evil, DayZ, Rust, Call of Duty, Battlefield V и другие.<br/>
+            <br/>
+            Мы вручную отбираем самые лучшие игры в наши кейсы. Наша задача: всегда стараться радовать вас качеством наших товаров!<br/>
+            <br/>
+            Вам не понравилась игра в этом кейсе? Пожалуйста, напишите её название здесь и мы уберем её из наших рандомов: https://vk.cc/cel2st<br/>
+            <br/>
+            Посмотрите наши отзывы за 8 лет работы: https://vk.com/topic-89671768_31757173<br/>
+            <br/>
+            Моментальное получение товара: vk.me/get_my_key<br/>`
         },
         {
             image: '/premium.jpg',
             title: 'Premium Random Key',
-            price: 225
+            price: 225,
+            description: `Из этого кейса вы получите игру от 299 до 2599 рублей.<br/>
+            Мы вернем ваши деньги, если игра в Steam окажется дешевле без учета скидок.<br/>
+            <br/>
+            Покупая Premium Random Key вы не только экономите деньги, но и имеете шанс получить дорогую игру, включая такие хиты, как: GTA V, Resident Evil, Rust, Terraria, DayZ, The Witcher, Stalker, Raft, Borderlands, For Honor, Stray и другие.<br/>
+            <br/>
+            Мы вручную отбираем самые лучшие игры в наши кейсы. Наша задача: всегда стараться радовать вас качеством наших товаров!<br/>
+            <br/>
+            Вам не понравилась игра в этом кейсе? Пожалуйста, напишите её название здесь и мы уберем её из наших рандомов: https://vk.cc/cel2st<br/>
+            <br/>
+            Посмотрите наши отзывы за 8 лет работы: https://vk.com/topic-89671768_31757173<br/>
+            <br/>
+            Моментальное получение товара: vk.me/get_my_key<br/>`
         },
         {
             image: '/narod.jpg',
             title: 'Народный Рандом',
-            price: 105
+            price: 105,
+            description: `Из этого кейса вы получите игру до 1999 рублей<br/>.
+            <br/>
+            Мы вручную отбираем самые лучшие игры в наши кейсы. Наша задача: всегда стараться радовать вас качеством наших товаров!<br/>
+            <br/>
+            Вам не понравилась игра в этом кейсе? Пожалуйста, напишите её название здесь и мы уберем её из наших рандомов: https://vk.cc/cel2st<br/>
+            <br/>
+            Посмотрите наши отзывы за 8 лет работы: https://vk.com/topic-89671768_31757173<br/>
+            <br/>
+            Моментальное получение товара: vk.me/get_my_key<br/>`
         }
     ]
 
@@ -37,10 +75,32 @@ export default function Index() {
                             <div className="text-white text-xl">{value.title}</div>
                             <div className="text-white font-bold">{value.price} RUB</div>
                         </div>
-                        <button className="text-white bg-slate-800 w-full rounded-md p-2 shadow-xl hover:bg-slate-700">Купить</button>
+                        <button onClick={() => {
+                            setIndex(index)
+                            setModalShow(true)
+                        }} className="text-white bg-slate-800 w-full rounded-md p-2 shadow-xl hover:bg-slate-700">Купить</button>
                     </div>
                 ))}
             </div>
+
+            {/* card view */}
+            {modalShow && 
+            <>
+                <div className={"fixed left-0 right-0 top-0 bottom-0 m-auto"} style={{backgroundColor: "rgba(0, 0, 0, 0.6)"}}>
+                <div className="fixed left-0 right-0 top-3 m-auto bg-slate-800 w-5/6 h-5/6 rounded-md p-5">
+                    <div className="text-white text-2xl font-bold">{items[index].title}</div>
+                    <div className="relative text-white text-sm overflow-y-auto h-5/6" dangerouslySetInnerHTML={{__html: items[index].description}}></div>
+                    {/* <div className="text-white font-bold pt-3 pb-3">{items[index].price} RUB</div> */}
+                    <div className="w-full flex flex-row gap-1 pt-3">
+                        <button onClick={() => {
+                            setModalShow(false)
+                        }} className="text-white bg-red-500 hover:bg-red-400 rounded-md shadow-xl w-full p-3">Отмена</button>
+                        <button className="text-white bg-green-500 hover:bg-green-400 rounded-md shadow-xl w-full p-3">Купить</button>
+                    </div>
+                    </div>
+                </div>
+            </>
+            }
         </>
     )
 }
